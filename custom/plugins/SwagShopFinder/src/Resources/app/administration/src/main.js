461e385d2637952a95e771289d0629f207eecec1
Hai Huynh <hai.huynh@hometogo.com>
2025-04-03 14:54:21 +0700

    update SwagShopFinder

--- custom/plugins/SwagShopFinder/src/Resources/app/administration/src/main.js
+++ custom/plugins/SwagShopFinder/src/Resources/app/administration/src/main.js
@@ -1,1 +1,7 @@
 import './module/swag-shop-finder';
+
+import template from './module/sw-product/page/sw-product-list/sw-product-list.html.twig';
+
+Shopware.Component.override('sw-product-list', {
+    template
+});
